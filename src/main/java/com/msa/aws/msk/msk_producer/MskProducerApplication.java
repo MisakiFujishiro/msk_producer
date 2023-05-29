2425b7c9ef74ecd447abdf2d629bde669ba42259
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-05-29 20:32:08 +0900

    mirroring test

--- src/main/java/com/msa/aws/msk/msk_producer/MskProducerApplication.java
+++ src/main/java/com/msa/aws/msk/msk_producer/MskProducerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MskProducerApplication {
 
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(MskProducerApplication.class, args);
 	}
