94d66348815f7afdf011702becbd28f423d027d8
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-06-08 21:10:13 +0900

    msk producer

--- src/main/java/com/msa/aws/msk/msk_producer/MskProducerApplication.java
+++ src/main/java/com/msa/aws/msk/msk_producer/MskProducerApplication.java
@@ -1,13 +1,23 @@
 package com.msa.aws.msk.msk_producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MskProducerApplication {
-
+	@Autowired
+	private MessageSender messageSender;
 	public static void main(String[] args) {
 		SpringApplication.run(MskProducerApplication.class, args);
+	}
+	public void run(String... args) {
+		if (args.length > 0) {
+			String message = args[0];
+			messageSender.sendMessage(message);
+		} else {
+			System.out.println("No message provided.");
+		}
 	}
 
 }
