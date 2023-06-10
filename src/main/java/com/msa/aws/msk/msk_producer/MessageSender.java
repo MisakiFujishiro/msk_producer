f2fc17690e0c1ec5b841d3a7214d9788b398c546
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-06-10 13:07:47 +0900

    msk producer

--- src/main/java/com/msa/aws/msk/msk_producer/MessageSender.java
+++ src/main/java/com/msa/aws/msk/msk_producer/MessageSender.java
@@ -3,20 +3,21 @@ package com.msa.aws.msk.msk_producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
 @Component
 public class MessageSender {
-
     private final KafkaTemplate<String, String> kafkaTemplate;
-    private final String topicName = "MSKTutorialTopic";
+    private final String topic;
 
     @Autowired
     public MessageSender(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
-        kafkaTemplate.setDefaultTopic(topicName);
+        this.topic = "MSKTutorialTopic"; // 送信先のトピック名
     }
 
     public void sendMessage(String message) {
-        kafkaTemplate.sendDefault(message);
-        System.out.println("Message sent: " + message);
+        kafkaTemplate.send(topic, message);
     }
 }
--- src/main/java/com/msa/aws/msk/msk_producer/MskProducerApplication.java
+++ src/main/java/com/msa/aws/msk/msk_producer/MskProducerApplication.java
@@ -1,27 +1,18 @@
 package com.msa.aws.msk.msk_producer;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class MskProducerApplication implements ApplicationRunner {
-	@Autowired
-	private MessageSender messageSender;
+public class MskProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MskProducerApplication.class, args);
-	}
+		ConfigurableApplicationContext context = SpringApplication.run(MskProducerApplication.class, args);
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		if (args.getSourceArgs().length > 0) {
-			String message = args.getSourceArgs()[0];
-			messageSender.sendMessage(message);
-		} else {
-			System.out.println("No message provided.");
-		}
+		MessageSender messageSender = context.getBean(MessageSender.class);
+		messageSender.sendMessage("Hello, Kafka!");
+
+		// アプリケーション終了時にコンテキストを閉じる
+		context.close();
 	}
 }
