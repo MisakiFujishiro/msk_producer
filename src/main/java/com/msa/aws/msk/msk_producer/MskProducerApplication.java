31ebc878b809fcd9a650c35a9581b76900fd48eb
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-07-05 11:44:33 +0900

    msk producer

--- src/main/java/com/msa/aws/msk/msk_producer/MskProducerApplication.java
+++ src/main/java/com/msa/aws/msk/msk_producer/MskProducerApplication.java
@@ -1,11 +1,12 @@
 package com.msa.aws.msk.msk_producer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MskProducerApplication {
+public class MskProducerApplication implements CommandLineRunner {
 	@Autowired
 	private MessageSender messageSender;
 	public static void main(String[] args) {
